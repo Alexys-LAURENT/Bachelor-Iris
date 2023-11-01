64ad90b47efd3340d5436e4d766e79e5f142c851
Alexys LAURENT <103429337+Alexys-LAURENT@users.noreply.github.com>
2023-11-02 00:49:22 +0100

    fix display and add link to conversations rows

--- ChatSphere/Views/ConversationsSection.php
+++ ChatSphere/Views/ConversationsSection.php
@@ -2,10 +2,10 @@
 
 $discussions = $unControleur->getDiscussionsDetails($user['idUser']);
 $colleagues = $unControleur->getAllColleagues($user['idUser']);
-//var_dump($discussions);
+// var_dump($discussions);
 
 if (isset($_POST['createDiscussion'])) {
-    
+
     //post members + $user['idUser'] in single array
     $unControleur->createDiscussion($_POST['NewDiscussionName'], array_merge($_POST['members'], array($user['idUser'])));
     header("Refresh:0");
@@ -22,73 +22,70 @@ if (isset($_POST['createDiscussion'])) {
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M12 4v16m8-8H4" />
             </svg>
         </button>
-            <!-- close drawer btn -->
-            <div class="block lg:hidden  ">
-                <svg onclick="toggleConv()" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
-                    <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
-                </svg>
-            </div>
+        <!-- close drawer btn -->
+        <div class="block lg:hidden  ">
+            <svg onclick="toggleConv()" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+                <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
+            </svg>
+        </div>
     </div>
 
     <div class="flex justify-center ">
         <!-- content -->
-        <div>
 
-            <div class="flex flex-col items-center gap-1 relative px-4">
-                <div class="flex w-14/14 mt-3 justify-between w-full">
-                    <input id="searchInput" type="text" placeholder="Rechercher un message" class="rounded-md bg-userMessage px-2 py-1 w-full focus:outline-primary cursor-text text-gray-600">
 
-                </div>
-        
+        <div class="flex flex-col items-center gap-1 relative px-4 w-full">
+            <div class="flex w-14/14 mt-3 justify-between w-full mb-3">
+                <input id="searchInput" type="text" placeholder="Rechercher un message" class="rounded-md bg-userMessage px-2 py-1 w-full focus:outline-primary cursor-text text-gray-600">
 
-           
-
-<?php
-foreach ($discussions as $discussion) {
-    echo '
-    <div class="w-full flex flex-col gap-2 hover:bg-hover hover:cursor-pointer rounded-md py-2 mx-16">
-        <!-- Contact row -->
-        <div class="flex max-w-full mx-4 gap-2">
-            <div class="aspect-square rounded-md bg-gray-500 w-[45px] h-[45px]"></div>
-            <div class="flex flex-col ">
-                <p class="font-semibold w-full text-elipsis line-clamp-1">' . $discussion['participants'] . '</p>
-                <span class="conversationRow w-full line-clamp-1 text-elipsis text-gray-500 text-xs relative top-[-3px]">' . $discussion['dernier_message'] . '</span>
             </div>
-        </div>
-    </div>';
-}
-?>
+
+            <?php
+            foreach ($discussions as $discussion) {
+                $class = $_GET['discussion'] == $discussion['idDiscussion'] ? 'bg-hover' : '';
+                echo '
+                <a class="m-0 p-0 w-full" href="?token=' . $_GET['token'] . '&discussion=' . $discussion['idDiscussion'] . '">
+                    <div class="w-full flex flex-col gap-2 hover:bg-hover hover:cursor-pointer rounded-md py-2 ' . $class . ' ">
+                        <!-- Contact row -->
+                        <div class="flex max-w-full mx-3 gap-2">
+                            <div class="aspect-square rounded-md bg-gray-500 w-[45px] h-[45px]"></div>
+                            <div class="flex flex-col ">
+                                <p class="font-semibold w-full text-elipsis line-clamp-1">' . $discussion['participants'] . '</p>
+                                <span class="conversationRow w-full line-clamp-1 text-elipsis text-gray-500 text-xs relative top-[-3px]">' . $discussion['dernier_message'] . '</span>
+                            </div>
+                        </div>
+                    </div>
+                </a>';
+            }
+            ?>
+
+            <?php require_once('popUpCreateDiscussion.php'); ?>
 
 
-
-
-                <?php require_once('popUpCreateDiscussion.php'); ?>
-
-
-                <script>
-                    // Fonction pour filtrer les messages
-                    function filterMessages() {
-                        // Récupérer la valeur de l'entrée de recherche
-                        const searchInput = document.getElementById("searchInput");
-                        const searchText = searchInput.value.toLowerCase();
-
-                        // Sélectionner tous les messages à filtrer (les spans contenant les messages)
-                        const messageSpans = document.querySelectorAll(".conversationRow");
-
-                        // Parcourir tous les messages et les afficher ou les masquer en fonction de la recherche
-                        messageSpans.forEach(span => {
-                            const messageText = span.textContent.toLowerCase();
-                            if (messageText.includes(searchText)) {
-                                span.parentElement.parentElement.parentElement.style.display = "flex"; // Afficher le message
-                            } else {
-                                span.parentElement.parentElement.parentElement.style.display = "none"; // Masquer le message
-                            }
-                        });
-                    }
-
-                    // Écouter les événements de changement dans l'entrée de recherche
+            <script>
+                // Fonction pour filtrer les messages
+                function filterMessages() {
+                    // Récupérer la valeur de l'entrée de recherche
                     const searchInput = document.getElementById("searchInput");
-                    searchInput.addEventListener("input", filterMessages);
-                </script>
+                    const searchText = searchInput.value.toLowerCase();
+
+                    // Sélectionner tous les messages à filtrer (les spans contenant les messages)
+                    const messageSpans = document.querySelectorAll(".conversationRow");
+
+                    // Parcourir tous les messages et les afficher ou les masquer en fonction de la recherche
+                    messageSpans.forEach(span => {
+                        const messageText = span.textContent.toLowerCase();
+                        if (messageText.includes(searchText)) {
+                            span.parentElement.parentElement.parentElement.style.display = "flex"; // Afficher le message
+                        } else {
+                            span.parentElement.parentElement.parentElement.style.display = "none"; // Masquer le message
+                        }
+                    });
+                }
+
+                // Écouter les événements de changement dans l'entrée de recherche
+                const searchInput = document.getElementById("searchInput");
+                searchInput.addEventListener("input", filterMessages);
+            </script>
 
 </aside>
