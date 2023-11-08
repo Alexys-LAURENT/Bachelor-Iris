6992f1eeb0709d54aa9fe9537d96c48d46f0ec74
Louis <louis.lamouche2204@gmail.com>
2023-11-08 01:29:25 +0100

    fix Stats button appearing when no discussion selected

--- ChatSphere/Views/MessagesSection.php
+++ ChatSphere/Views/MessagesSection.php
@@ -44,8 +44,8 @@ if (isset($_POST['deleteDiscussion'])) {
             </div>
         </div>
 
-        <div onclick="toggleStats()" class="min-w-[50px] flex">
-            <button class="w-full flex  justify-center items-center">
+        <div onclick="toggleStats()" class="min-w-[50px] flex <?php if (!isset($discussionInfo)) echo 'hidden' ?> ">
+            <button class="w-full flex justify-center items-center">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-bar-chart-line" viewBox="0 0 16 16">
                     <path d="M11 2a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v12h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3h1V7a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v7h1V2zm1 12h2V2h-2v12zm-3 0V7H7v7h2zm-5 0v-3H2v3h2z" />
                 </svg>
