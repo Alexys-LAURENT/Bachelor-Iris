60334c465a9fdb6047c9ba839539e2ded6c8a34b
Alexys LAURENT <103429337+Alexys-LAURENT@users.noreply.github.com>
2023-12-06 11:21:37 +0100

    init controller and model

---
+++ NoteFlow/src/main/java/Model/Model.java
@@ -0,0 +1,129 @@
+package Model;
+
+// import java.util.ArrayList;
+// import java.sql.ResultSet;
+// import java.sql.SQLException;
+// import java.sql.Statement;
+
+public class Model {
+    // private static Connexion maConnexion = new Connexion("localhost:3306",
+    // "orange_sd_24", "root", "");
+
+    /******************* GESTION DES CLIENTS *******************/
+    // public static void insertClient(Client unClient) {
+    // String req = "insert into client values(null,'"
+    // + unClient.getNom() + "','"
+    // + unClient.getPrenom() + "','"
+    // + unClient.getAdresse() + "');";
+    // try {
+    // maConnexion.seConnecter();
+    // Statement unStat = maConnexion.getMaConnexion().createStatement();
+    // unStat.execute(req);
+    // unStat.close();
+    // maConnexion.seDeconnecter();
+    // } catch (SQLException exp) {
+    // System.out.println("Erreur d'execution : " + req + " : " + exp);
+    // }
+
+    // }
+
+    // public static ArrayList<Client> selectAllClients() {
+    // String req = "select * from client;";
+    // ArrayList<Client> lesClients = new ArrayList<Client>();
+    // try {
+    // maConnexion.seConnecter();
+    // Statement unStat = maConnexion.getMaConnexion().createStatement();
+    // ResultSet desRes = unStat.executeQuery(req);
+    // while (desRes.next()) {
+    // Client unClient = new Client(desRes.getInt("idclient"),
+    // desRes.getString("nom"),
+    // desRes.getString("prenom"), desRes.getString("adresse"));
+    // lesClients.add(unClient);
+    // }
+    // unStat.close();
+    // maConnexion.seDeconnecter();
+    // } catch (SQLException exp) {
+    // System.out.println("Erreur d'execution : " + req + " : " + exp);
+    // }
+
+    // return lesClients;
+    // }
+
+    // public static void deleteClient(int idclient) {
+    // String req = "delete from client where idclient = " + idclient + ";";
+    // try {
+    // maConnexion.seConnecter();
+    // Statement unStat = maConnexion.getMaConnexion().createStatement();
+    // unStat.execute(req);
+    // unStat.close();
+    // maConnexion.seDeconnecter();
+    // } catch (SQLException exp) {
+    // System.out.println("Erreur d'execution : " + req + " : " + exp);
+    // }
+    // }
+
+    // public static Client selectWhereClient(int idclient) {
+    // String req = "select * from client where idclient = " + idclient + ";";
+    // Client unClient = null;
+    // try {
+    // maConnexion.seConnecter();
+    // Statement unStat = maConnexion.getMaConnexion().createStatement();
+    // ResultSet unRes = unStat.executeQuery(req);
+    // if (unRes.next()) {
+    // unClient = new Client(
+    // unRes.getInt("idclient"),
+    // unRes.getString("nom"),
+    // unRes.getString("prenom"),
+    // unRes.getString("adresse"));
+    // }
+    // unStat.close();
+    // maConnexion.seDeconnecter();
+    // } catch (SQLException exp) {
+    // System.out.println("Erreur d'execution : " + req + " : " + exp);
+    // }
+
+    // return unClient;
+    // }
+
+    // public static void updateClient(Client unClient) {
+    // String req = "update client set "
+    // + "nom = '" + unClient.getNom() + "',"
+    // + "prenom = '" + unClient.getPrenom() + "',"
+    // + "adresse = '" + unClient.getAdresse() + "' "
+    // + "where idclient = " + unClient.getIdclient() + ";";
+    // try {
+    // maConnexion.seConnecter();
+    // Statement unStat = maConnexion.getMaConnexion().createStatement();
+    // unStat.execute(req);
+    // unStat.close();
+    // maConnexion.seDeconnecter();
+    // } catch (SQLException exp) {
+    // System.out.println("Erreur d'execution : " + req + " : " + exp);
+    // }
+    // }
+
+    // public static ArrayList<Client> selectLikeClients(String filtre) {
+    // String req = "select * from client where "
+    // + "nom like '%" + filtre + "%' "
+    // + "or prenom like '%" + filtre + "%' "
+    // + "or adresse like '%" + filtre + "%';";
+    // ArrayList<Client> lesClients = new ArrayList<Client>();
+    // try {
+    // maConnexion.seConnecter();
+    // Statement unStat = maConnexion.getMaConnexion().createStatement();
+    // ResultSet desRes = unStat.executeQuery(req);
+    // while (desRes.next()) {
+    // Client unClient = new Client(desRes.getInt("idclient"),
+    // desRes.getString("nom"),
+    // desRes.getString("prenom"), desRes.getString("adresse"));
+    // lesClients.add(unClient);
+    // }
+    // unStat.close();
+    // maConnexion.seDeconnecter();
+    // } catch (SQLException exp) {
+    // System.out.println("Erreur d'execution : " + req + " : " + exp);
+    // }
+
+    // return lesClients;
+    // }
+}
